d5aaaa25fd598a93b0a1ea5e734f0ce59b6930ce
prabhu6899 <prabhu06.krishnan@gmail.com>
2021-04-09 12:59:15 +0530

    Raw File 1
    Iam inseting this new code

---
+++ Raw file 2.js
@@ -0,0 +1,23 @@
+00Q1P00000yN2BM - MQL - HPS EMEA PRODUCTS PMC ME KSA FI PI MODSY
+   
+<?xml version="1.0" encoding="UTF-8"?>
+<Profile xmlns="http://soap.sforce.com/2006/04/metadata">
+   <objectPermissions>
+        <allowCreate>true</allowCreate>
+        <allowDelete>false</allowDelete>
+        <allowEdit>true</allowEdit>
+        <allowRead>true</allowRead>
+        <modifyAllRecords>false</modifyAllRecords>
+        <object>Lead</object>
+        <viewAllRecords>false</viewAllRecords>
+    </objectPermissions>
+	  <objectPermissions>
+        <allowCreate>true</allowCreate>
+        <allowDelete>false</allowDelete>
+        <allowEdit>true</allowEdit>
+        <allowRead>true</allowRead>
+        <modifyAllRecords>false</modifyAllRecords>
+        <object>Lead</object>
+        <viewAllRecords>false</viewAllRecords>
+    </objectPermissions>
+	</Profile>
